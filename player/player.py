b11c7776fbe36e8b35f3aecf2d49ef1f38cc5895
leota0608 <xichen.tao1@rbe.sk.ca>
2025-12-19 13:45:10 -0600

    1.1.3

--- player/player.py
+++ player/player.py
@@ -22,11 +22,26 @@ class Player:
         finally:
             print("Personal information loading...")
             time.sleep(0.7)
+        self.store_player_information()
 
-    def get_name():
+    def get_name(self):
         while True:
             string = input("Write down your name: ")
             confirm = input("Is this really your name?(y/n)")
             if(confirm.lower() == "y" or confirm.lower() == "yes"):
                 return string
+
+    def store_player_information(self):
+        # add try and except
+        with open("playingRecord.json", 'r') as file:
+            record = json.load(file)
+        total_num = record["Total Player"]
+        record["Total Player"] += 1
+        record[str(total_num+1)] = {"Name": self.name,
+                                    "Date": self.date,
+                                    "Score": self.score}
+        with open("playingRecord.json", 'w') as file:
+            json.dump(record, file, indent = 4)
+
+player = Player()
 
